81906cc85f0d46e1896778ab5645f179ad687f18
LiuZhe <miuiclub@qq.com>
2017-06-23 00:33:25 +0800

    搭建好的项目，尝试展示滑动界面，failed

---
+++ src/main/scala/com/green/MainActivity.scala
@@ -0,0 +1,60 @@
+package com.green
+
+import java.util
+
+import android.app.Activity
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.graphics.drawable.Animatable
+import android.support.v4.view.{PagerAdapter, ViewPager}
+import android.view.{LayoutInflater, View}
+
+class MainActivity extends AppCompatActivity {
+    // allows accessing `.value` on TR.resource.constants
+    implicit val context = this
+
+    //    四个布局
+    private var connectView: View = _
+    private var purchaseView : View = _
+    private var helpView : View = _
+    private var optionView : View = _
+
+    private var viewPager: ViewPager = _
+    private var viewList: util.ArrayList[View] = _
+
+    override def onCreate(savedInstanceState: Bundle): Unit = {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main)
+
+//        初始化
+        viewPager = findViewById(R.id.viewpager).asInstanceOf[ViewPager]
+        val inflater : LayoutInflater = getLayoutInflater()
+        connectView = inflater.inflate(R.layout.connect,null)
+        purchaseView = inflater.inflate(R.layout.purchase,null)
+        helpView = inflater.inflate(R.layout.help,null)
+        optionView = inflater.inflate(R.layout.option,null)
+
+//        将要分页显示的View装入数组中
+        viewList = new util.ArrayList[View]()
+        viewList.add(connectView)
+        viewList.add(purchaseView)
+        viewList.add(helpView)
+        viewList.add(optionView)
+
+//        适配器部分
+        val pagerAdapter : NewPagerAdapter = new NewPagerAdapter(viewList)
+        viewPager.setAdapter(pagerAdapter)
+    }
+
+
+    class NewPagerAdapter(viewList: util.ArrayList[View]) extends PagerAdapter{
+        override def getCount: Int = {
+            viewList.size()
+        }
+
+        override def isViewFromObject(view: View, o: scala.Any): Boolean = {
+            view == o
+        }
+    }
+
+}
