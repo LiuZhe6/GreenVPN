eaf4a45ab95e055a6287889b6f0bf97ab3c7746d
LiuZhe <miuiclub@qq.com>
2017-07-01 14:19:11 +0800

    7-1

--- src/main/scala/com/green/MainActivity.scala
+++ src/main/scala/com/green/MainActivity.scala
@@ -2,11 +2,15 @@ package com.green
 
 import java.util
 
-import android.app.{ActionBar, Activity}
+import android.app.{ActionBar, Activity, FragmentManager}
 import android.content.Context
 import android.os.Bundle
+import android.support.design.widget.TabLayout.Tab
 import android.support.v7.app.AppCompatActivity
 import android.support.design.widget.{TabItem, TabLayout}
+import android.support.v4.app
+import android.support.v4.app.Fragment
+import android.support.v4.view.ViewPager.OnPageChangeListener
 import android.support.v4.view.{PagerAdapter, ViewPager}
 import android.view.{LayoutInflater, View, ViewGroup}
 import android.widget._
@@ -24,13 +28,14 @@ class MainActivity extends AppCompatActivity  with TabLayout.OnTabSelectedListen
 
   private var viewPager: ViewPager = _
   private var viewList: util.ArrayList[View] = _
+  private var fragmentList : util.ArrayList[Fragment] = _
 
   //  四个标签 和 TabLayout
   private var tabLayout : TabLayout = _
-  private var connectTab: TabItem = _
-  private var purchaseTab: TabItem = _
-  private var helpTab: TabItem = _
-  private var optionTab: TabItem = _
+  private var connectTab: Tab = _
+  private var purchaseTab: Tab= _
+  private var helpTab: Tab = _
+  private var optionTab: Tab = _
 
   //页面适配器
   private var pagerAdapter: PagerAdapter = _
@@ -43,58 +48,79 @@ class MainActivity extends AppCompatActivity  with TabLayout.OnTabSelectedListen
   var childList : util.ArrayList[String] = _
   private var listViewAdapter : MyExpandableViewAdapter = _
 
+  var connectFragment : ConnectFragment = _
+  var purchaseFragment : PurchaseFragment = _
+  var helpFragment : HelpFragment = _
+  var optionFragment : OptionFragment = _
 
 
-  /**
-    * 初始化控件
-    *
-    * @return
-    */
-  def init(): Unit = {
-
-    //初始化总页面的布局
-    initIndex()
-
-    //初始化help页面的布局
-//    initHelpPage()
-
-  }
-
-  /**
-    * 初始化总页面布局
-    */
-  def initIndex():Unit = {
-    viewPager = findViewById(R.id.viewpager).asInstanceOf[ViewPager]
-    val inflater: LayoutInflater = getLayoutInflater()
-    connectView = inflater.inflate(R.layout.connect, null)
-    purchaseView = inflater.inflate(R.layout.purchase, null)
-    helpView = inflater.inflate(R.layout.help, null)
-    optionView = inflater.inflate(R.layout.option, null)
-    //        将要分页显示的View装入数组中
-    viewList = new util.ArrayList[View]()
-    viewList.add(connectView)
-    viewList.add(purchaseView)
-    viewList.add(helpView)
-    viewList.add(optionView)
-
-    //标签初始化
+  def initView() = {
+    connectFragment  = new ConnectFragment
+    purchaseFragment = new PurchaseFragment
+    helpFragment = new HelpFragment
+    optionFragment = new OptionFragment
     tabLayout = findViewById(R.id.tabLayout).asInstanceOf[TabLayout]
-    connectTab = findViewById(R.id.connectTab).asInstanceOf[TabItem]
-    println("1" + tabLayout)
-    purchaseTab = findViewById(R.id.purchaseTab).asInstanceOf[TabItem]
-    helpTab = findViewById(R.id.helpTab).asInstanceOf[TabItem]
-    optionTab = findViewById(R.id.optionTab).asInstanceOf[TabItem]
+    viewPager = findViewById(R.id.viewpager).asInstanceOf[ViewPager]
 
+    fragmentList = new util.ArrayList[Fragment]()
+    fragmentList.add(connectFragment)
+    fragmentList.add(purchaseFragment)
+    fragmentList.add(helpFragment)
+    fragmentList.add(optionFragment)
 
-    //        适配器部分 and tabLayout适配viewPager
-    pagerAdapter = new NewPagerAdapter(viewList)
-    viewPager.setAdapter(pagerAdapter)
+    val fragmentManager : app.FragmentManager = this.getSupportFragmentManager()
+    viewPager.setAdapter(new MainFragmentPagerAdapter(fragmentManager,fragmentList))
+    //默认显示连接页面
+    viewPager.setCurrentItem(0)
+
+    //设置tabLayout与viewPager级联
     tabLayout.setupWithViewPager(viewPager)
-    tabLayout.getTabAt(0).setText("连接")
-    tabLayout.getTabAt(1).setText("购买")
-    tabLayout.getTabAt(2).setText("帮助")
-    tabLayout.getTabAt(3).setText("选项")
+
+    //实例化Tab
+    connectTab = tabLayout.getTabAt(0)
+    purchaseTab = tabLayout.getTabAt(1)
+    helpTab = tabLayout.getTabAt(2)
+    optionTab = tabLayout.getTabAt(3)
+
+    //重新设置标签名称，上面会清除全部标签
+    connectTab.setText("连接")
+    purchaseTab.setText("购买")
+    helpTab.setText("帮助")
+    optionTab.setText("选项")
+
   }
+  def initEvents() = {
+
+    tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener {
+
+      override def onTabSelected(tab: Tab): Unit = {
+        if (tab == tabLayout.getTabAt(0)){
+          viewPager.setCurrentItem(0)
+        } else if (tab == tabLayout.getTabAt(1)){
+          viewPager.setCurrentItem(1)
+        } else if (tab == tabLayout.getTabAt(2)){
+          viewPager.setCurrentItem(2)
+        } else if (tab == tabLayout.getTabAt(3)){
+          viewPager.setCurrentItem(3)
+        }
+      }
+
+      override def onTabUnselected(tab: Tab): Unit = Unit
+
+      override def onTabReselected(tab: Tab): Unit = Unit
+    })
+  }
+
+  override def onCreate(savedInstanceState: Bundle): Unit = {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.index)
+
+
+    initView()
+    initEvents()
+
+  }
+
 
   /**
     * 初始化help页面的布局
@@ -134,20 +160,11 @@ class MainActivity extends AppCompatActivity  with TabLayout.OnTabSelectedListen
   }
 
 //  以下三个方法是Tab事件
-  override def onTabSelected(tab: TabLayout.Tab): Unit = ???
+  override def onTabSelected(tab: TabLayout.Tab): Unit = Unit
 
-  override def onTabReselected(tab: TabLayout.Tab): Unit = ???
+  override def onTabReselected(tab: TabLayout.Tab): Unit = Unit
 
-  override def onTabUnselected(tab: TabLayout.Tab): Unit = ???
-
-  override def onCreate(savedInstanceState: Bundle): Unit = {
-    super.onCreate(savedInstanceState)
-    setContentView(R.layout.index)
-
-    //        全局初始化函数,初始化控件
-    init()
-
-  }
+  override def onTabUnselected(tab: TabLayout.Tab): Unit = Unit
 
 
   //滑动页面适配器
@@ -299,5 +316,30 @@ class MainActivity extends AppCompatActivity  with TabLayout.OnTabSelectedListen
       */
     override def getGroupId(parentPosition: Int): Long = parentPosition
 
+  }
+
+
+
+  //TabItem的监听事件
+  class MyOnClickListener(i:Int) extends View.OnClickListener{
+
+    private val index = i
+
+    override def onClick(view: View): Unit = viewPager.setCurrentItem(index)
+  }
+
+
+  /**
+    * 页面切换监听，改变动画位置
+    */
+  class MyOnChangeListener extends OnPageChangeListener{
+
+    override def onPageSelected(i: Int): Unit = {
+
+    }
+
+    override def onPageScrolled(i: Int, v: Float, i1: Int): Unit = ???
+
+    override def onPageScrollStateChanged(i: Int): Unit = ???
   }
 }
---
+++ src/main/scala/com/green/PurchaseFragment.scala
@@ -0,0 +1,18 @@
+package com.green
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.{LayoutInflater, View, ViewGroup}
+
+/**
+  * Created by coder on 17-6-30.
+  */
+class PurchaseFragment extends Fragment{
+
+
+  override def onCreateView(inflater: LayoutInflater, container: ViewGroup, savedInstanceState: Bundle): View = {
+    super.onCreateView(inflater, container, savedInstanceState)
+    val v : View = inflater.inflate(R.layout.purchase,container,false)
+    v
+  }
+}
