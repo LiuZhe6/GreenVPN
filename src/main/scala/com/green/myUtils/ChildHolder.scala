a41f9f66c8c62124264e7804a25570f7b39b3d07
LiuZhe <miuiclub@qq.com>
2017-07-01 17:33:57 +0800

    实现help界面

---
+++ src/main/scala/com/green/myUtils/ChildHolder.scala
@@ -0,0 +1,13 @@
+package com.green.myUtils
+
+import android.widget.TextView
+
+
+/**
+  * Created by coder on 17-7-1.
+  */
+class ChildHolder(t : TextView) {
+
+  val textView : TextView = t
+
+}
---
+++ src/main/scala/com/green/myUtils/GroupHolder.scala
@@ -0,0 +1,13 @@
+package com.green.myUtils
+
+import android.graphics.Color
+import android.widget.TextView
+
+/**
+  * Created by coder on 17-7-1.
+  */
+class GroupHolder(t:TextView) {
+
+  var textView: TextView = t
+
+}
